e77dba65769a8f335f20f5ba3b57481ba39f80c0
Stefan Iloaie <stefan.iloaie@3pillarglobal.com>
2024-08-19 12:45:01 +0300

    BEN-2526 fix for is_ajax call

--- filer/utils/status.py
+++ filer/utils/status.py
@@ -6,6 +6,9 @@ from django.contrib.sites.models import Site
 
 from filer.models import File, Folder
 
+def is_ajax(request):
+    """Checks if a request is ajax."""
+    return request.headers.get('x-requested-with') == 'XMLHttpRequest'
 
 def chunked_items(qs, size=100):
     pk = 0
@@ -162,4 +165,3 @@ class FileChecker(object):
             log_progress(stats)
 
         return stats
-
