3450af1645975876bf1e1deede8f12ab58e80260
Andrei Lupuleasa <andrei.lupuleasa@3pillarglobal.com>
2025-04-03 15:19:06 +0300

    BEN-2998: show filer version

--- filer/apps.py
+++ filer/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
+from filer import __version__ as filer_version
 
 
 class FilerConfig(AppConfig):
     name = 'filer'
-    verbose_name = _("Filer")
+    verbose_name = _(f"Filer ({filer_version})")
