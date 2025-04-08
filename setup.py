a9ff5194233e2e906a0f1b0c45a060fa50b668ad
Stefan Iloaie <stefan.iloaie@3pillarglobal.com>
2025-04-08 10:03:40 +0300

    BEN-3182

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=(
         'django>=4.2',
         'django-mptt>=0.6,<1.0',  # the exact version depends on Django
-        'django_polymorphic==3.1.0',
+        'django_polymorphic==4.0.0',
         'django-js-asset==2.0.0',
         'easy-thumbnails>=2,<3.0',
         'Unidecode>=0.04,<1.2',
