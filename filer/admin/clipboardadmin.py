e6f4d3b4eaf2f52cac13b7bd14765779996dc090
Stefan Ioan Iloaie <stefan.iloaie@3pillarglobal.com>
2025-06-25 10:44:02 +0300

    BEN-3182 fix

--- filer/admin/clipboardadmin.py
+++ filer/admin/clipboardadmin.py
@@ -33,7 +33,7 @@ class ClipboardItemInline(admin.TabularInline):
 class ClipboardAdmin(admin.ModelAdmin):
     model = Clipboard
     inlines = [ClipboardItemInline]
-    # filter_horizontal = ('files',)
+    filter_horizontal = ('files',)
     raw_id_fields = ('user',)
     verbose_name = "DEBUG Clipboard"
     verbose_name_plural = "DEBUG Clipboards"
